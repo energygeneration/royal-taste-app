ee7b176aba6c4dc1ae78aff8e214a7ff6bdc8a9d
estherella12 <estherbatawila@gmail.com>
2022-09-21 20:59:42 +0000

    ameloiration du frontend et gestion de la responsiviter

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,11 +20,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/css.min.css" rel="stylesheet">
     <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="./asset/css/esther.css" rel="stylesheet">
+    <link href="./asset/css/royal.css" rel="stylesheet">
 </head>
 
 <body>
@@ -55,28 +55,22 @@
 
     <!-- Navbar Start -->
     <div class="container-fluid position-relative nav-bar p-0">
-        <div class="container-lg position-relative p-0 px-lg-3" style="z-index: 9;">
+        <div class="navposition position-relative p-0 px-lg-3" style="z-index: 9;">
             <nav class="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
-                <!-- <a href="{{route('index')}}" class="navbar-brand d-block d-lg-none">
+                <a href="{{route('index')}}" class="navbar-brand d-block d-lg-none">
                     <h1 class="m-0 display-4 text-primary"><span class="text-secondary">ROYAL</span>TASTE</h1>
-                </a> -->
+                </a>
                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                    <div class="navbar-nav ml-auto py-0">
+                    <div class="navbar-nav ml-auto px-4">
                         <a href="{{route('index')}}" class="nav-item nav-link active">Acceuil</a>
                         <a href="{{route('patisserie')}}" class="nav-item nav-link">Patisserie</a>
                         <a href="{{route('traiteur')}}" class="nav-item nav-link">Service traiteur</a>
-                        <a href="index.html" class="navbar-brand mx-5 d-none d-lg-block">
-                        <h1 class="m-0 display-4 text-primary"><span class="text-secondary">ROYAL</span>TASTE</h1>
-                    </a>
-                    <a href="{{route('formulaire')}}" class="nav-item nav-link">Commander en ligne</a>
-                        <a href="{{ route('login') }}" class="nav-item nav-link">Login</a>
-                    </div>
-                   
-                    <div class="navbar-nav mr-auto py-0">
-                        
+                         <h3 style="margin-top: 30px;"> <span class="text-secondary" >ROYAL</span><span style="color: #F195B2">TASTE</span></h3>
+                        <a href="{{route('formulaire')}}" class="nav-item nav-link">Commander en ligne</a>
+                        <a href="{{ route('login') }}" class="nav-item nav-link ">Login</a>
                     </div>
                 </div>
             </nav>
@@ -95,7 +89,7 @@
                         <div class="p-3" style="max-width: 900px;">
                             <h4 class="text-white text-uppercase mb-md-3">Traditionelle & Delicieuse</h4>
                             <h1 class="display-3 text-white mb-md-4">Une Patisserie proche de vous</h1>
-                            <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2">Commander</a>
+                            <a href="{{route('commandez')}}" class="btn btn-primary py-md-3 px-md-5 mt-2">Commander</a>
                         </div>
                     </div>
                 </div>
@@ -103,9 +97,9 @@
                     <img class="w-100" src="./asset/img/carousel-1.jpg" alt="Image">
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3" style="max-width: 900px;">
-                            <h4 class="text-white text-uppercase mb-md-3">Traditionelle & Delicieuse</h4>
-                            <h1 class="display-3 text-white mb-md-4">ROYALTASTE,votre Patisserie proche de vous</h1>
-                            <a href="" class="btn btn-primary py-md-3 px-md-5 mt-2">Commander</a>
+                            <h4 class="text-white text-uppercase mb-md-3">Une Patisserie proche de vous</h4>
+                            <h1 class="display-3 text-white mb-md-4">Made From ROYALTASTE</h1>
+                            <a href="{{route('commandez')}}" class="btn btn-primary py-md-3 px-md-5 mt-2">Commander</a>
                         </div>
                     </div>
                 </div>
@@ -156,7 +150,7 @@
                     <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>Cupcake</h5>
                     <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>Mousse au chocolat</h5>
                     <h5 class="text-muted mb-3"><i class="fa fa-check text-secondary mr-3"></i>etc..</h5>
-                    <a href="" class="btn btn-primary mt-2">Commander</a>
+                    <a href="{{route('commandez')}}" class="btn btn-primary mt-2">Commander</a>
                 </div>
             </div>
         </div>
@@ -180,31 +174,12 @@
                     </div>
                     <h3 class="font-weight-bold text-white mt-3 mb-4">Chocolat- Vanille-Fraise</h3>
                     <p class="text-white mb-4">Toutes les saveurs disponible juste pour répondre a vos bésoins et celle de vos petits enfants</p>
-                    <a href="" class="btn btn-secondary py-3 px-5 mt-2">Commander</a>
+                    <a href="{{route('commandez')}}" class="btn btn-secondary py-3 px-5 mt-2">Commander</a>
                 </div>
             </div>
         </div>
     </div>
     <!-- Promotion End -->
-
-
-    <!-- Video Modal Start -->
-    <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-body">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>        
-                    <!-- 16:9 aspect ratio -->
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="" id="video"  allowscriptaccess="always" allow="autoplay"></iframe>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Video Modal End -->
 
 
     <!-- Services Start -->
@@ -228,7 +203,7 @@
                                     <div class="card-body">
                                       <h5 class="card-title">Gâteau mousse mangue</h5>
                                       <p class="card-text">Commandez vos gâteau à partir de 6500F.</p>
-                                      <a href="#" class="btn">commandez</a>
+                                      <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                     </div>
                                   </div>
                                 </div>
@@ -238,7 +213,7 @@
                                     <div class="card-body">
                                       <h5 class="card-title">Forêt noir</h5>
                                       <p class="card-text">Forêt noir à la chantily <br> 25000f/35000f</p>
-                                      <a href="#" class="btn">commandez</a>
+                                      <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                     </div>
                                   </div>
                                 </div>
@@ -249,7 +224,7 @@
                                       <h5 class="card-title">Cup cake</h5>
                                       <p class="card-text">Cup cake<br>
                                        3500f(6)</p>
-                                      <a href="#" class="btn">commandez</a>
+                                      <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                     </div>
                                   </div>
                                 </div>
@@ -265,7 +240,7 @@
                                   <div class="card-body">
                                     <h5 class="card-title">happy birthday </h5>
                                     <p class="card-text"> Gâteau pour adule <br> 20000f/25000f</p>
-                                    <a href="#" class="btn">commandez</a>
+                                    <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                   </div>
                                 </div>
                               </div>
@@ -275,7 +250,7 @@
                                   <div class="card-body">
                                     <h5 class="card-title">Mousse au chocolat</h5>
                                     <p class="card-text">Mousse au chocolat <br> 1500f/2500f</p>
-                                    <a href="#" class="btn">commandez</a>
+                                    <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                   </div>
                                 </div>
                               </div>
@@ -285,7 +260,7 @@
                                   <div class="card-body">
                                     <h5 class="card-title">Gâteau de marriage</h5>
                                     <p class="card-text">Personaliser vos gâteaux en fonction de vos budjet</p>
-                                    <a href="#" class="btn">commandez</a>
+                                    <a href="{{route('commandez')}}" class="btn btn-primary">commandez</a>
                                   </div>
                                 </div>
                               </div>
@@ -297,15 +272,15 @@
                           <!--------------------------------   notre service traiteur    ------------------------>
                       
                       
-                         <div class="d-flex mt-5 partie2">
+                         <div class="partie2 d-flex mt-5 ">
                            <div class="imge-traiteur">
-                             <img src="./asset/img/buffet-6329757_1920.jpg" width="700" alt="">
+                             <img src="./asset/img/buffet-6329757_1920.jpg" class="taiteur" width="700" alt="">
                            </div>
                            <div class="text-partie2">
                              <p>Nous vous accompagnons également pour tous vos différents événements d’un buffet complét et d'un dessert gourmand.</p>
                            </div>
                          </div>
-                         <button type="submit" class="btn commander-traiteur ">reservez nos services</button>
+                         <a href="{{route('formulaire')}}" class="btn-reserve btn-secondary commander-traiteur fs-1 px-3">Résèrvez nos services</a>
                       </div>
                     </div>
                 </div>
@@ -328,24 +303,28 @@
                     <div class="owl-carousel testimonial-carousel">
                         <div class="text-center">
                             <i class="fa fa-3x fa-quote-left text-primary mb-4"></i>
-                            <h4 class="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                            <h4 class="font-weight-light mb-4">"Je tenais à vous remercier, les gâteaux était magnifiques et délicieux.
+                                Une véritable surprise, ma mère était très contente et émue.
+                                Encore merci, je vous re-contacterais pour un futur événement."</h4>
                             <img class="img-fluid mx-auto mb-3" src="img/testimonial-1.jpg" alt="">
-                            <h5 class="font-weight-bold m-0">Client Name</h5>
-                            <span>Profession</span>
+                            <h5 class="font-weight-bold m-0">Sophie</h5>
                         </div>
                         <div class="text-center">
                             <i class="fa fa-3x fa-quote-left text-primary mb-4"></i>
-                            <h4 class="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                            <h4 class="font-weight-light mb-4">Je recommande vivement cake design by annie Kone.
+                                 A plusieurs reprise nous somme passé par elle pour les anniversaires des enfants jamais déçu gâteau aussi beau que bon.
+                                 Une vrai merveille pour les yeux et les papilles</h4>
                             <img class="img-fluid mx-auto mb-3" src="img/testimonial-2.jpg" alt="">
-                            <h5 class="font-weight-bold m-0">Client Name</h5>
-                            <span>Profession</span>
+                            <h5 class="font-weight-bold m-0">Lydie Rose</h5>
                         </div>
                         <div class="text-center">
                             <i class="fa fa-3x fa-quote-left text-primary mb-4"></i>
-                            <h4 class="font-weight-light mb-4">Dolor eirmod diam stet kasd sed. Aliqu rebum est eos. Rebum elitr dolore et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam</h4>
+                            <h4 class="font-weight-light mb-4">"Un très grand merci pour votre gâteau qui était magnifique et qui a fait un sacré effet pour 
+                                l'anniversaire de Théa!!Vraiment un grand Bravo...très beau et très bon...la framboise c'était parfait!!A bientôt!"
+                                "Un très grand merci pour votre gâteau qui était magnifique et qui a fait un sacré effet pour l'anniversaire de Théa!!Vraiment 
+                                un grand Bravo...très beau et très bon...la framboise c'était parfait!!A bientôt!"</h4>
                             <img class="img-fluid mx-auto mb-3" src="img/testimonial-3.jpg" alt="">
-                            <h5 class="font-weight-bold m-0">Client Name</h5>
-                            <span>Profession</span>
+                            <h5 class="font-weight-bold m-0">Gwladys</h5>
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/commandez.blade.php
+++ resources/views/layouts/commandez.blade.php
@@ -1,54 +1,86 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>patisserie</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>ROYALTASTE</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/css/patisserie.css">
+
     <link rel="stylesheet" href="./asset/css/style.css">
     <link rel="stylesheet" href="./asset/css/formulaire.css">
-  </head>
-  <body>
 
-       <!-------------------------------- navbar ------------------------>
-  <body>
-    <div class="header">
-      <div class="header-color d-flex">
-        <div class="contact">
-          <div class="icon-contact d-flex">
-            <img src="./asset/img/Vector__1_-removebg-preview.png" alt="">
-            <div><p class="text-light">00228 92339749</p></div>
-          </div>
-        </div>
-        <div class="email">
-          <div class="icon-contact d-flex">
-            <img src="./asset/img/fontisto_email-removebg-preview.png" alt="">
-            <div><p class="text-light"><p class="text-light">mail@domaine.com</p></div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <nav class="navbar navbar-expand-lg bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><span>ROYAL</span><span class="tast">TASTE</span></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="{{route('index')}}">Acceuil</a>
-            <a class="nav-link" href="{{route('patisserie')}}">Patisserie</a>
-            <a class="nav-link" href="{{route('traiteur')}}">Service traiteur</a>
-            <a class="nav-link"  href="{{route('formulaire')}}">Commander en ligne </a>
-            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-          </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    </div>
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
-  <!-------------------------------- fin navbar ------------------------>
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="./asset/css/royal.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-primary py-3 d-none d-md-block">
+        <div class="container">
+            <div class="row">
+            
+                <div class="col-md-6 text-center text-lg-right">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a>
+                        <a class="text-white px-3" href="">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+  <!-- Navbar Start -->
+  <div class="container-fluid position-relative nav-bar p-0">
+        <div class="navposition position-relative p-0 px-lg-3" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light shadow p-lg-0">
+                <a href="{{route('index')}}" class="navbar-brand d-block d-lg-none">
+                    <h1 class="m-0 display-4 text-primary"><span class="text-secondary">ROYAL</span>TASTE</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto px-4">
+                        <a href="{{route('index')}}" class="nav-item nav-link active">Acceuil</a>
+                        <a href="{{route('patisserie')}}" class="nav-item nav-link">Patisserie</a>
+                        <a href="{{route('traiteur')}}" class="nav-item nav-link">Service traiteur</a>
+                        <h3 style="margin-top: 30px;"> <span class="text-secondary" >ROYAL</span><span style="color: #F195B2">TASTE</span></h3>
+                        <a href="{{route('formulaire')}}" class="nav-item nav-link">Commander en ligne</a>
+                        <a href="{{ route('login') }}" class="nav-item nav-link ">Login</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
 
       <!-- Formulaire -->
       <div class="form-postulant" id="form-inscription">
@@ -130,3 +162,63 @@
        
     </div>
     <!-- Formulaire -->
+
+    <!-- Footer Start -->
+<div class="container-fluid footer bg-light py-5" style="margin-top: 90px;">
+    <div class="container text-center py-5">
+        <div class="row">
+            <div class="col-12 mb-4">
+                <a href="index.html" class="navbar-brand m-0">
+                    <h1 class="m-0 mt-n2 display-4 text-primary"><span class="text-secondary">ROYAL</span>TASTE</h1>
+                </a>
+            </div>
+            <div class="col-12 mb-4">
+                <a class="btn btn-outline-secondary btn-social mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-outline-secondary btn-social mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-secondary btn-social" href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="col-12 mt-2 mb-4">
+                <div class="row">
+                    <div class="col-sm-6 text-center text-sm-right border-right mb-3 mb-sm-0">
+                        <h5 class="font-weight-bold mb-2">CONTACT</h5>
+                        <p class="mb-2">Vakpossito fish <br> Lomé-Togo</p>
+                        <p class="mb-0"><br>Tel: 00228 92339749 <br> 00228 97270517</p></p>
+                    </div>
+                    <div class="col-sm-6 text-center text-sm-left">
+                        <h5 class="font-weight-bold mb-2">HORAIRE</h5>
+                        <p class="mb-2">Lundi – Samedi, 09h – 19h</p>
+                        <p class="mb-0">Dimanche, 09h - 21H </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <p class="m-0"> Copyright © 2022</p>
+                
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-secondary px-2 back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/isotope/isotope.pkgd.min.js"></script>
+<script src="lib/lightbox/js/lightbox.min.js"></script>
+
+<!-- Contact Javascript File -->
+<script src="mail/jqBootstrapValidation.min.js"></script>
+<script src="mail/contact.js"></script>
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+</body>
+</html>
